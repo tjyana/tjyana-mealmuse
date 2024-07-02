de13c60f252b95f662e2b2c1c492639a0bb370cb
TJ Yanagitsuru <taro.yanagitsuru@gmail.com>
2024-07-02 20:24:17 +0900

    Revert "took out all backgrounds"
    This reverts commit ae5e66510d9e7d0e6b5ab05547f59bc4bea60346.
    
    trying to revert

--- app.py
+++ app.py
@@ -309,9 +309,9 @@ if st.session_state['page3']:
     col2.markdown("<h2 style='text-align: center; color: grey; font-size:20px'>Turn What You Have to What You Crave &#128512; </h2>", unsafe_allow_html=True)
     #col2.markdown("<h3 style='text-align: center; color: black; font-size:10px'>Enter Your Ingredients</h3>", unsafe_allow_html=True)
 
-    # st.image('Streamlit/pages/mealmuse_images/circleobjectleft.png')
-    # st.image('Streamlit/pages/mealmuse_images/circleobjectright.png')
-    # st.image('Streamlit/pages/mealmuse_images/centerobjects.png')
+    st.image('Streamlit/pages/mealmuse_images/circleobjectleft.png')
+    st.image('Streamlit/pages/mealmuse_images/circleobjectright.png')
+    st.image('Streamlit/pages/mealmuse_images/centerobjects.png')
 
 
     # Load model
